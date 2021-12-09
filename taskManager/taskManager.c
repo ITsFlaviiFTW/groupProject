0bc3d93d699be4d40004b59211a8767e29a00d5f
Hayden Auterhoff <haydenauterhoff20@gmail.com>
2021-12-09 16:54:27 -0500

    taskManager.c code

---
+++ taskManager/taskManager.c
@@ -0,0 +1,71 @@
+#include "Header.h"
+
+
+void addTask()
+{
+    int val;
+    struct node* ptr = (struct node*)malloc(sizeof(struct node));
+    if (ptr == NULL)
+    {
+        printf("not able to add the task to the list");
+    }
+    else
+    {
+        printf("Enter the task: ");
+        scanf_s("%d", &val);
+        if (head == NULL)
+        {
+            ptr->val = val;
+            ptr->next = NULL;
+            head = ptr;
+        }
+        else
+        {
+            ptr->val = val;
+            ptr->next = head;
+            head = ptr;
+
+        }
+        printf("Task added to the list!");
+
+    }
+}
+
+void deleteTask()
+{
+    int item;
+    struct node* ptr;
+    if (head == NULL)
+    {
+        printf("No task available to delete!\n");
+    }
+    else
+    {
+        item = head->val;
+        ptr = head;
+        head = head->next;
+        free(ptr);
+        printf("Task has been deleted");
+
+    }
+}
+
+void displayTask()
+{
+    int i;
+    struct node* ptr;
+    ptr = head;
+    if (ptr == NULL)
+    {
+        printf("Task List is empty\n");
+    }
+    else
+    {
+        printf("Printing the task list\n");
+        while (ptr != NULL)
+        {
+            printf("%d\n", ptr->val);
+            ptr = ptr->next;
+        }
+    }
+}
