e1a65998e529a7955c0631a4980842d2a6e7b879
Hayden Auterhoff <haydenauterhoff20@gmail.com>
2021-12-09 16:51:10 -0500

    Source.c code

---
+++ taskManager/Source.c
@@ -0,0 +1,59 @@
+#include "Header.h"
+
+void printOptions() {
+
+    printf("Welcome to your task manager\n");
+    printf("a) Add new task\n");
+    printf("b) Delete an existing task\n");
+    printf("c) Update an existing task\n");
+    printf("d) Display a single task\n");
+    printf("e) Display a range of tasks\n");
+    printf("f) Display all tasks\n");
+    printf("g) Search for a task\n");
+    printf("h) Quit\n");
+
+}
+
+int main() {
+
+    char inputKey;
+    do {
+        printOptions();
+
+        printf("Please enter the operation number for the desired option: ");
+        scanf_s("%c", &inputKey);
+        printf("\n");
+
+        switch (inputKey)
+        {
+        case 'a':
+            addTask();
+            break;
+        case 'b':
+            deleteTask();
+            break;
+        case 'c':
+            //Update an existing task
+            break;
+        case 'd':
+            // Display a single task
+            break;
+        case 'e':
+            // Display a range of tasks
+            break;
+        case 'f':
+            displayTask();
+            break;
+        case 'g':
+            //Search for a task
+            break;
+        case 'h':
+            //Quit
+            break;
+
+        }
+
+    } while (inputKey != 'h');
+
+
+}
