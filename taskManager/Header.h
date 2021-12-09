5a362287c949258084c0780ca6874c0a2dfc465c
Hayden Auterhoff <haydenauterhoff20@gmail.com>
2021-12-09 16:56:17 -0500

    header.h

--- taskManager/Header.h
+++ taskManager/Header.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct node
+{
+    int val;
+    struct node* next;
+};
+struct node* head;
+
+void printOptions();
+void addTask();
+void deleteTask();
+void displayTask();
